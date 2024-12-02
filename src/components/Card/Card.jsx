324e971cae01228efdde042c82fa3cf0c199e60b
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-02 15:38:11 +0100

    ADD: repo

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import style from './Card.module.css'
+import placeHolderImage from '../../assets/Img/600x400_placeholder.jpg'
+import Button from '../ui/Button/Button.jsx'
+import Tags from '../Tags/Tags.jsx'
+
+
+export default function Card({ title = '', image, content = '', tags = [] }) {
+
+    return (
+        <>
+            <div className="container">
+                <div className={style.card}>
+                    <figure className={style.image}>
+                        <img className={style.thumb} src={image || placeHolderImage} alt={title} />
+                    </figure>
+                    <div className={style.body}>
+                        <h3 className={style.title}>{title}</h3>
+                        < Tags tags={tags} />
+                        <p className={style.description}>{content}</p>
+                        <Button />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,48 @@
+import Card from './Card/Card.jsx'
+import { posts } from '../data/posts.js'
+import Tags from './Tags/Tags.jsx'
+
+export default function Main() {
+    const uniqueTags = [];
+    for (const post of posts) { // per ogni post dell'array di oggetti posts
+        for (const tag of post.tags) { // per ogni array di tags di ogni post
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag); // se il tag se non è già presente lo pusho
+            }
+        }
+    }
+
+    const publishedPosts = posts.filter((post) => post.published === true) // meglio fare con filter, per fare poi meno iterazioni dopo
+    return (
+        <>
+            <main>
+                <section className='posts_section'>
+                    <div className='container'>
+                        <h1 className='page-title'>Il mio blog</h1>
+                        <div className='tags_stripe'>
+                            < Tags tags={uniqueTags} />
+                        </div>
+                    </div>
+                    <div className='container'>
+                        {publishedPosts.length ? ( // Se ci sono post
+                            <div className="row">
+                                {publishedPosts.map((post) =>
+                                    <div key={post.id} className="col-6">
+                                        <Card
+                                            image={post.image}
+                                            title={post.title}
+                                            tags={post.tags}
+                                            content={post.content}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <p>No posts available</p> // Se l'array `posts` è vuoto
+                        )}
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
